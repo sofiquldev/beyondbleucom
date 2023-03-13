2e460eeb2bd7a139cd4bf812857d4dc4a35d75ef
asgerdeveloper <127490290+asgerdeveloper@users.noreply.github.com>
2023-03-13 14:43:22 +0900

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -63,7 +63,6 @@ export default () => {
                 info@beyondbleu.com
               </a>
             </div>
-            <br />
             <div>
               <a
                 href="https://www.instagram.com/beyondbleu_psychotherapy/"
