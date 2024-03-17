8f4eab0549d4fc3ca5b8df2eecaa08aa2be5b63e
beyondbleu <129825088+beyondbleu@users.noreply.github.com>
2024-03-17 11:32:11 +0600

    Update index.en.js for <frame> or <iframe> elements do not have a title

--- src/pages/couples-therapy/index.en.js
+++ src/pages/couples-therapy/index.en.js
@@ -91,7 +91,7 @@ const Index = props => {
             </div>
 <div class="aspect-w-16 aspect-h-9 mx-auto">
   <div class="w-full" style={{height:'400px'}}>
-    <iframe class=" w-full h-full" src="https://www.youtube.com/embed/Ap1TxCYZCHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe title="Imagine how you and your partner would feel if you" class=" w-full h-full" src="https://www.youtube.com/embed/Ap1TxCYZCHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
 </div>
 
